06742484b2e4f3487a94264867d12fff40f0596f
Haonn <74655091+Haonn@users.noreply.github.com>
2020-12-09 11:12:51 +0100

    Update NGUYENPaul_python.py

--- NGUYENPaul_python.py
+++ NGUYENPaul_python.py
@@ -1,5 +1,7 @@
 motAleat = ["C", "R", "A", "Y", "O", "N"]
 lettre = ["X","X","X","X","X","X"]
+lettreCorrecte = [0,0,0,0,0,0]
+lettrePresente = [0,0,0,0,0,0]
 def choix (lettre):
     for i in range (0,6) :
         if i == 0 :
@@ -10,5 +12,14 @@ def choix (lettre):
         lettre[i] = input()
     return lettre
     
+def correcte (lettre,motAleat,lettreCorrecte):
+	for i in range (0,6) :
+		if (lettre[i] == motAleat[i]) :
+			lettreCorrecte[i]=+1
+	return lettreCorrecte
+
 choix(lettre)
 print (lettre)
+correcte (lettre, motAleat, lettreCorrecte)
+print(lettreCorrecte)
+input()
